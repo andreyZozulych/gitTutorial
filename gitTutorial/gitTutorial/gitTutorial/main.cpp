787fc3b80b27dad31f33b0eec3b5e26856f98006
Andrey Zozulych <it.andbussines@gmail.com>
2021-01-05 11:15:32 +0200

    Add project xCode

---
+++ gitTutorial/gitTutorial/gitTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitTutorial
+//
+//  Created by Andrey Zozulych on 05.01.2021.
+//  Copyright © 2021 Andrey Zozulych. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
